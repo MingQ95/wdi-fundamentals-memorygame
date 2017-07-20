ddb87b33c32f1daaf696332e169dcbbfffe40d6d
Ma <jot155@dca904764743.cof.ds.capitalone.com>
2017-07-20 14:26:22 -0400

    finished basic features, added reset button and scoreboard

--- js/main.js
+++ js/main.js
@@ -21,24 +21,63 @@ var cards = [
   }
 ];
 var cardsInPlay = [];
+var score = 0;
+
+var updateScore = function() {
+  scoreDisplay = "Score: " + score;
+  document.getElementsByTagName('h3')[0].textContent = scoreDisplay;
+}
 
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
+    score++;
+    updateScore();
   } else {
     alert("Sorry, try again.");
   }
-}
+  emptyCards();
+};
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+  this.setAttribute('src', cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
-    checkForMatch();
+    setTimeout(checkForMatch, 200);
+  }
+};
+
+var emptyCards = function () {
+  while (cardsInPlay.length > 0) {
+    cardsInPlay.pop();
   }
 }
 
-flipCard(0);
-flipCard(2);
+var resetBoard = function() {
+  emptyCards();
+  for (var i = 0; i < 4; ++i) {
+    var child = document.getElementsByTagName('img')[0];
+    document.getElementById('game-board').removeChild(child);
+  }
+  score = 0;
+  updateScore();
+  createBoard();
+}
+
+var createBoard = function() {
+  document.getElementsByTagName('button')[0].addEventListener('click',resetBoard);
+  for (var i = 0; i < cards.length; ++i) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+
+  }
+};
+
+createBoard();
